98341a7e92043b70bc3a4a010454baed0c705a4b
abhinav <abhinavreddynimma@gmail.com>
2018-05-09 15:53:11 +0530

    function named correction() added ; so as to re request all the url for which we were unable to execute function named courses()

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import requests
 from bs4  import BeautifulSoup
 import csv
+import time
 
 def urlg(query,pageno):
 
@@ -17,27 +18,29 @@ def degrees(s) :
     w.writerow(['','','','',''])
     ss=s.find('div',"rc-DegreesUpsell")
     sss=ss.findAll('div')
-    print (ss.h3.text+" : "+'\n')
+    #print (ss.h3.text+" : "+'\n')
     i=0
     while i<len(sss) :
         i=i+5
         t1=str(sss[i].text)
-        print(t1 +':')
+        #print(t1 +':')
         i=i+1
         t2=str(sss[i].p.text)
-        print(t2 +'\n')
+        #print(t2 +'\n')
         i=i+1
         w.writerow(['Degrees / Certificates',t1,t2,'',''])
     return len(sss)/7
 
-def courses(s1,d) :
+def courses(s1,d,l,n) :
 
         ss1=s1.find('div',"rc-SearchResults bt3-col-xs-9")
         try :
             sss=ss1.findAll('div')
         except :
-            print(str(s1)+'\n')
-            print(str(ss1)+'\n')
+            #print(str(s1)+'\n')
+            #print(str(ss1)+'\n')
+            print("error :"+str(n)+str(r))
+            l.append(n)
             return
             #print(str(sss)+'\n')
         #print(str(len(sss))+'\n')
@@ -55,10 +58,10 @@ def courses(s1,d) :
 
         while i<len(sss):
             courselink = "https://www.coursera.org" +sss[i].a.get('href')
-            print(courselink)
+            #print(courselink)
             i=i+5
             coursetitle=sss[i].h2.text
-            print(coursetitle)
+            #print(coursetitle)
             i=i+1
             s4=sss[i].findAll('span')
             if len(s4)==1:
@@ -72,46 +75,74 @@ def courses(s1,d) :
             w.writerow([ty,coursetitle,courselink,provider,spc])
             i=i+2
 
-
+def correction(l):
+    l1=[]
+    for i in l :
+        url = urlg(query,i)
+        r=requests.get(url)
+        print('requests done')
+        s=BeautifulSoup(r.text,'lxml')
+        print("BeautifulSoup done ")
+        courses(s,0,l1,i)
+    return l1
 
 query = " "
-    #query.replace(' ','-')
+         #query.replace(' ','-')
+print("code running")
 with open(query+'course details.csv','w') as outputfile :
+    print("file opened")
     w =csv.writer(outputfile)
     w.writerow(['Course Type','Course Title','Course Summary','Provider','specialisation'])
-    print (urlg(query,1)+'\n')
+
+
+    print ("url = "+urlg(query,1)+'\n')
     #r=requests.get(url(query,0))
     #s=BeautifulSoup(r.text,'lxml')
     #print ("lastpage : "+str(lastpage(s))
     url = urlg(query,1)
-    print(url)
+    #print(url)
     r=requests.get(url)
-    #print('requests done')
+    print('requests done')
     s=BeautifulSoup(r.text,'lxml')
-    #print("BeautifulSoup done ")
+    print("BeautifulSoup done ")
     lp=lastpage(s)
     print("last page = "+str(lp))
     d=0
     w.writerow(['','','','',''])
     w.writerow(['','','','',''])
     w.writerow(['','','','',''])
-    courses(s,d)
+    l=[]
+    courses(s,d,l,1)
     n=2
 
     while n < lp:
+     print('\n')
+     print('\n')
+     print(str(n))
+     print('\n')
+     print('\n')
      url = urlg(query,n)
      print("URL generated")
      r=requests.get(url)
      print('requests done')
      s=BeautifulSoup(r.text,'lxml')
      print("BeautifulSoup done ")
-     courses(s,d)
+     courses(s,d,l,n)
      n=n+1
-     print('\n')
-     print('\n')
-     print(str(n))
-     print('\n')
-     print('\n')
 
 
-print("not an infinity loop")
+        # l=[3, 16, 21, 57, 60, 65, 68, 104, 118, 119]
+    print(len(l))
+    print(l)
+    print("not an infinity loop")
+    print("correcting")
+    l1=correction(l)
+    print("correction done")
+    print(l1)
+    print(len(l1))
+    print("-----------------------------")
+    while len(l1)!=0:
+        #sleep(100)
+        l1=correction(l1)
+        print(l1)
+        print(len(l1))

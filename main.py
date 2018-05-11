89bfb1b658fac0a8d848a1e6c1e3292b9b498701
abhinav <abhinavreddynimma@gmail.com>
2018-05-11 15:09:36 +0530

    added code for rotating user agents

--- main.py
+++ main.py
@@ -2,6 +2,35 @@ import requests
 from bs4  import BeautifulSoup
 import csv
 import time
+import random
+
+user_agent_list = [
+#Chrome
+'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.113 Safari/537.36',
+'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.90 Safari/537.36',
+'Mozilla/5.0 (Windows NT 5.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.90 Safari/537.36',
+'Mozilla/5.0 (Windows NT 6.2; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.90 Safari/537.36',
+'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/44.0.2403.157 Safari/537.36',
+'Mozilla/5.0 (Windows NT 6.3; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.113 Safari/537.36',
+'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/57.0.2987.133 Safari/537.36',
+'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/57.0.2987.133 Safari/537.36',
+'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/55.0.2883.87 Safari/537.36',
+'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/55.0.2883.87 Safari/537.36',
+#Firefox
+'Mozilla/4.0 (compatible; MSIE 9.0; Windows NT 6.1)',
+'Mozilla/5.0 (Windows NT 6.1; WOW64; Trident/7.0; rv:11.0) like Gecko',
+'Mozilla/5.0 (compatible; MSIE 9.0; Windows NT 6.1; WOW64; Trident/5.0)',
+'Mozilla/5.0 (Windows NT 6.1; Trident/7.0; rv:11.0) like Gecko',
+'Mozilla/5.0 (Windows NT 6.2; WOW64; Trident/7.0; rv:11.0) like Gecko',
+'Mozilla/5.0 (Windows NT 10.0; WOW64; Trident/7.0; rv:11.0) like Gecko',
+'Mozilla/5.0 (compatible; MSIE 9.0; Windows NT 6.0; Trident/5.0)',
+'Mozilla/5.0 (Windows NT 6.3; WOW64; Trident/7.0; rv:11.0) like Gecko',
+'Mozilla/5.0 (compatible; MSIE 9.0; Windows NT 6.1; Trident/5.0)',
+'Mozilla/5.0 (Windows NT 6.1; Win64; x64; Trident/7.0; rv:11.0) like Gecko',
+'Mozilla/5.0 (compatible; MSIE 10.0; Windows NT 6.1; WOW64; Trident/6.0)',
+'Mozilla/5.0 (compatible; MSIE 10.0; Windows NT 6.1; Trident/6.0)',
+'Mozilla/4.0 (compatible; MSIE 8.0; Windows NT 5.1; Trident/4.0; .NET CLR 2.0.50727; .NET CLR 3.0.4506.2152; .NET CLR 3.5.30729)']
+
 
 def urlg(query,pageno):
         p=pageno-1
@@ -77,8 +106,14 @@ def courses(s1,d,l,n) :
 def correction(l):
     l1=[]
     for i in l :
+        for i in range(1,6):
+        #Pick a random user agent
+            user_agent = random.choice(user_agent_list)
+        headers = {'User-Agent': user_agent}
+
         url = urlg(query,i)
-        r=requests.get(url)
+        #r=requests.get(url)
+        r = requests.get(url,headers=headers)
         print('requests done')
         s=BeautifulSoup(r.text,'lxml')
         print("BeautifulSoup done ")
@@ -100,7 +135,13 @@ with open(query+'course details.csv','w') as outputfile :
     #print ("lastpage : "+str(lastpage(s))
     url = urlg(query,1)
     #print(url)
-    r=requests.get(url)
+    for i in range(1,6):
+    #Pick a random user agent
+        user_agent = random.choice(user_agent_list)
+    headers = {'User-Agent': user_agent}
+    r = requests.get(url,headers=headers)
+    #r=requests.get(url)
+
     print('requests done')
     s=BeautifulSoup(r.text,'lxml')
     print("BeautifulSoup done ")
@@ -122,12 +163,21 @@ with open(query+'course details.csv','w') as outputfile :
      print('\n')
      url = urlg(query,n)
      print("URL generated")
-     r=requests.get(url)
+     #r=requests.get(url)
+     for i in range(1,6):
+     #Pick a random user agent
+         user_agent = random.choice(user_agent_list)
+     headers = {'User-Agent': user_agent}
+     r = requests.get(url,headers=headers)
      print('requests done')
      s=BeautifulSoup(r.text,'lxml')
      print("BeautifulSoup done ")
+
+     sleep(r)
      courses(s,d,l,n)
      n=n+1
+      r=random.randint(0,10)
+      print("sleeping for "+str)
 
 
         # l=[3, 16, 21, 57, 60, 65, 68, 104, 118, 119]
